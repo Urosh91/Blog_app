2c9ec6936d1f8f3a095559bd4834b2114c0843bd
Urosh91 <urosh43@gmail.com>
2017-07-24 20:28:56 +0200

    production update

--- Blog_app/settings/__init__.py
+++ Blog_app/settings/__init__.py
@@ -1,10 +1,10 @@
 from .base import *
 
-try:
-    from .local import *
-    live = False
-except:
-    live = True
-
-if live:
-    from .production import *
+# try:
+#     from .local import *
+#     live = False
+# except:
+#     live = True
+#
+# if live:
+from .production import *
