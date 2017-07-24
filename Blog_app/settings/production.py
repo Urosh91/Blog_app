ca78a23a9b917afe79154e598e075154daf88c4b
Urosh91 <urosh43@gmail.com>
2017-07-24 20:15:17 +0200

    production update

--- Blog_app/settings/production.py
+++ Blog_app/settings/production.py
@@ -8,10 +8,11 @@ TEMPLATE_DEBUG = True
 
 DATABASES = settings.DATABASES
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
