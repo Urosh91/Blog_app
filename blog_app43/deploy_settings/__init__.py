4f853b5e5787d3a023e62c395cf3c8e07c922160
Urosh91 <urosh43@gmail.com>
2017-07-28 22:59:15 +0200

    prod

--- blog_app43/deploy_settings/__init__.py
+++ blog_app43/deploy_settings/__init__.py
@@ -2,24 +2,24 @@ import dj_database_url
 from blog_app43.settings import *
 
 DEBUG = False
-# TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = False
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates',],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-            'debug': False,
-        },
-    },
-]
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': ['templates',],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#             'debug': False,
+#         },
+#     },
+# ]
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -32,4 +32,4 @@ SECRET_KEY = get_env_variable('SECRET_KEY')
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
