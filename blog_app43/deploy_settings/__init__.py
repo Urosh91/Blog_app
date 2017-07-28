72e671b80294fb1f6bedfa9bb25416f9b6ad1702
Urosh91 <urosh43@gmail.com>
2017-07-28 22:49:08 +0200

    whitenoise

--- blog_app43/deploy_settings/__init__.py
+++ blog_app43/deploy_settings/__init__.py
@@ -2,7 +2,24 @@ import dj_database_url
 from blog_app43.settings import *
 
 DEBUG = False
-TEMPLATE_DEBUG = False
+# TEMPLATE_DEBUG = False
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['templates',],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'debug': False,
+        },
+    },
+]
 
 ALLOWED_HOSTS = [
     'localhost',
